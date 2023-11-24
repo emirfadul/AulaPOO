8a5a3e8946d125bec03cf9e528ef9a7cd81de24c
emirfadul <emirfadul@gmail.com>
2023-11-24 01:05:25 -0400

    Aprendendo THREADs, muito bom!

--- src/cursojava/thread/AulaThread.java
+++ src/cursojava/thread/AulaThread.java
@@ -1,14 +1,38 @@
 package cursojava.thread;
 
-public class AulaThread {
+import javax.swing.JOptionPane;
+
+public abstract class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
-		
-		for (int pos = 0; pos < 10; pos++) {
-			
-			Thread.sleep(2000);
-			System.out.println("Executando uma rotina!");
-		}
+
+		new Thread() {
+
+			public void run() {
+
+				for (int pos = 0; pos < 5; pos++) {
+
+					System.out.println((pos + 1) + ") Executando uma rotina!");
+
+					try {
+
+						Thread.sleep(2000);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				}
+
+				System.out.println("==== FIM ====");
+
+				JOptionPane.showMessageDialog(null, "Fim do processso !", "Alerta", JOptionPane.WARNING_MESSAGE);
+
+			}
+
+		}.start();
+
+		JOptionPane.showMessageDialog(null, "Sistema continua executando....", "Alerta", JOptionPane.ERROR_MESSAGE);
 	}
 
 }

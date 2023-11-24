405917cd60190480e874e8b033c6d9591fca6a59
emirfadul <emirfadul@gmail.com>
2023-11-24 11:34:10 -0400

    Thread Runnable

--- src/cursojava/thread/AulaThread.java
+++ src/cursojava/thread/AulaThread.java
@@ -6,33 +6,66 @@ public abstract class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		new Thread() {
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 
-			public void run() {
+		Thread threadNF = new Thread(thread2);
+		threadNF.start();
 
-				for (int pos = 0; pos < 5; pos++) {
-
-					System.out.println((pos + 1) + ") Executando uma rotina!");
-
-					try {
-
-						Thread.sleep(2000);
-
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-
-				}
-
-				System.out.println("==== FIM ====");
-
-				JOptionPane.showMessageDialog(null, "Fim do processso !", "Alerta", JOptionPane.WARNING_MESSAGE);
-
-			}
-
-		}.start();
 
 		JOptionPane.showMessageDialog(null, "Sistema continua executando....", "Alerta", JOptionPane.ERROR_MESSAGE);
 	}
+	
+	private static Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			
+			for (int pos = 0; pos < 5; pos++) {
 
+				System.out.println((pos + 1) + ") Executando envio EMAIL!");
+
+				try {
+
+					Thread.sleep(2000);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			System.out.println("==== FIM ====");
+
+			JOptionPane.showMessageDialog(null, "Fim do envio de EMAIL", "Alerta", JOptionPane.WARNING_MESSAGE);
+
+		}
+	};
+	
+	private static Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			for (int pos = 0; pos < 5; pos++) {
+
+				System.out.println((pos + 1) + ") Executando NOTA FISCAL");
+
+				try {
+
+					Thread.sleep(2000);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			System.out.println("==== FIM ====");
+
+			JOptionPane.showMessageDialog(null, "Fim do envio de NOTA FISCAL", "Alerta",
+					JOptionPane.WARNING_MESSAGE);
+
+		}
+	};
+	
 }
